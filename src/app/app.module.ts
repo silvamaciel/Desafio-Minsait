ec658e19749b28a48e54189a0e3b992df6df142b
silvamaciel <macieljunio.si@gmail.com>
2023-10-27 20:55:51 -0300

    projeto inicializado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProdutosComponent } from './components/produtos/produtos.component';
+import { CadastroComponent } from './components/cadastro/cadastro.component';
+import { EditarComponent } from './components/editar/editar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProdutosComponent,
+    CadastroComponent,
+    EditarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
